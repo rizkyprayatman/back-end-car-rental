4c239ad6fe6702217ccd84c2aae209ec11ae022c
Rizky Prayatman <r.prayatman@gmail.com>
2022-12-19 12:01:58 +0800

    add file back end car rental

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const route = require('./src/router/index.js');
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const app = express();
+const port = 8005;
+
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Binar Cars Express API with Swagger",
+      version: "0.1.0",
+      description: "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Binar Car",
+        url: "https://binarcar.netlify.app/",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: `http://localhost:${port}/api/`,
+      },
+    ],
+  },
+  apis: ["./src/router/car.js", "./src/router/user.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
+app.use('/api', route(express));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
